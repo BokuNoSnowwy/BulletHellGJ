58fcdc503ec3368f3f9141cc0709f37dc0f8e862
NeZmaI <mateovita@outlook.fr>
2024-02-20 15:15:39 +0100

    world borders camera

--- Assets/Camera/CameraManager.cs
+++ Assets/Camera/CameraManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -14,9 +16,17 @@ public class CameraManager : MonoBehaviour
 
     [Space(10)]
 
-    [Header("Ground and camera's target")]
+    [Header("Camera's target")]
     [SerializeField] private Transform _target;
+    [SerializeField] private float movementThreshold = 0.05f;
+
+    [Space(10)]
+
+    [Header("Ground and camera's target")]
+    public bool UseSpriteForBounds = false;
     [SerializeField] private SpriteRenderer _ground;
+    public Vector2 WorldSize = new Vector2(1f,1f);
+    public Vector2 WorldOrigin = new Vector2(0f,0f);
 
     private float _width, _height;
 
@@ -44,10 +54,16 @@ public class CameraManager : MonoBehaviour
             SetOrthographicSize();
         }
 
-        UpdateCameraPosition();
+        if(Vector2.Distance(transform.position, _target.position) > movementThreshold)
+        {
+            UpdateCameraPosition();
+        }
     }
 
     #region OrthographicSize
+    /// <summary>
+    /// Sets the orthographic size of the camera depending on the device's screen resolution
+    /// </summary>
     public void SetOrthographicSize()
     {
         float orthoSize = 0.0f;
@@ -66,19 +82,48 @@ public class CameraManager : MonoBehaviour
     #endregion 
 
     #region CameraMovements
+
+    /// <summary>
+    /// Calculates camera view limits.
+    /// </summary>
     public void CalculateBorders()
     {
-        if (_ground != null)
+        //Note : _camera.ViewportToWorldPoint(new Vector3(1, 1, _camera.nearClipPlane)) = camera top right corner 
+        //       _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane)) = camera bottom left corner 
+
+        Vector3 topRightCorner = _camera.ViewportToWorldPoint(new Vector3(1, 1, _camera.nearClipPlane));
+        Vector3 bottomLeftCorner = _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+
+        if (UseSpriteForBounds && _ground != null)
         {
             Vector3 extents = _ground.bounds.extents;
 
-            _maxX = (_ground.transform.position.x + extents.x - _camera.ViewportToWorldPoint(new Vector3(1, 1, _camera.nearClipPlane)).x);
-            _maxY = (_ground.transform.position.y + extents.y - _camera.ViewportToWorldPoint(new Vector3(1, 1, _camera.nearClipPlane)).y);
-            _minX = (_ground.transform.position.x - extents.x - _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane)).x);
-            _minY = (_ground.transform.position.y - extents.y - _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane)).y);
+            _maxX = _ground.transform.position.x + extents.x - topRightCorner.x;
+
+            _maxY = _ground.transform.position.y + extents.y - topRightCorner.y;
+
+            _minX = _ground.transform.position.x - extents.x - bottomLeftCorner.x;
+
+            _minY = _ground.transform.position.y - extents.y - bottomLeftCorner.y;
+        }
+        else
+        {
+            float halfWorldSizeX = WorldSize.x / 2;
+            float halfWorldSizeY = WorldSize.y / 2;
+
+            _maxX = WorldOrigin.x + halfWorldSizeX - topRightCorner.x;
+
+            _maxY = WorldOrigin.y + halfWorldSizeY - topRightCorner.y;
+
+            _minX = WorldOrigin.x - halfWorldSizeX - bottomLeftCorner.x;
+
+            _minY = WorldOrigin.y - halfWorldSizeY - bottomLeftCorner.y;
         }
     }
 
+    /// <summary>
+    /// Updates the camera position
+    /// </summary>
     private void UpdateCameraPosition()
     {
         Vector3 newPos = Vector2.Lerp(transform.position,_target.position, _speed * Time.deltaTime);
@@ -88,7 +133,11 @@ public class CameraManager : MonoBehaviour
 
         transform.position = newPos;
     }
-
+    
+    /// <summary>
+    /// Checks if the given position isn't out of world's bounds and corrects it
+    /// </summary>
+    /// <param name="newPos">The position's reference</param>
     private void CheckBorders(ref Vector3 newPos)
     {
         if(newPos.x < _minX)
@@ -108,5 +157,14 @@ public class CameraManager : MonoBehaviour
             newPos.y = transform.position.y;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //debug cube to see world border size
+        Gizmos.DrawWireCube(WorldOrigin, WorldSize);
+    }
 }
 #endregion 
+
